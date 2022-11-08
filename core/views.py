6258d7e1a99385a42f089ab369fef4ffcd382f2b
gusmendez99 <gus.mendez.99@gmail.com>
2022-11-08 12:46:29 -0600

    [REF] template html

--- core/views.py
+++ core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-def index (request):
-   return render(request, 'app/index.html')
-
 def home(request):
-   return render(request, 'app/index.html')
+   return render(request, 'core/index.html')
